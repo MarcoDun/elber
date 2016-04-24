e70d103a4329dd3b56383e727f98cdfa865c08be
MarcoDun <duntianxing@163.com>
2016-04-23 23:41:29 -0700

    add all the connected pages and add iron-router to the package

--- home/elber.js
+++ home/elber.js
@@ -1,3 +1,24 @@
+Router.route('/', function () {
+      this.render('membership');
+    });
+
+Router.route('/about', function() {
+	this.render('about');
+});
+
+Router.route('/faq', function() {
+	this.render('faq');
+});
+
+Router.route('/signup', function() {
+	this.render('signup');
+});
+
+Router.route('/login', function() {
+	this.render('login');
+});
+
+
 if (Meteor.isClient) {
   
 }
