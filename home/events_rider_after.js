7403ab0db56aa394a7abfeb89a798853c3415548
MarcoDun <duntianxing@163.com>
2016-04-28 23:38:32 -0500

    add dashboard after schedule ride/ take ride

---
+++ home/events_rider_after.js
@@ -0,0 +1,47 @@
+// This is the Database of Upcoming Events
+// Please Edit with Care.
+//
+// 8 Fields (surrounded by brackets[]) are used for EACH event:
+// 	["Recurring", "Month", "Day", "Year", "StartTime", "EndTime", "Name", "Description"]
+// 	Each event field must be be surrounded by quotation marks followed by a comma ("",) EXCEPT the "Description" field.
+//	The "Description" field is surrounded by quotation marks only ("").
+//
+// Each event has a comma after the closing bracket IF another event is below it on the next line down.
+//	Note: The last event in this file should NOT have a comma after the closing bracket
+//
+// The Recurring field uses:
+//	"D" = Daily; "W" = Weekly; "M" = Monthly; "Y" = Yearly; "F" = Floating Holiday
+//
+// One Time only events should leave the Recurring field blank
+//	(ex. "")
+//
+// Daily events do NOT require that anything be in the Month Day and Year fields
+//	Everything in the Month Day and Year fields will be ignored
+//
+// Weekly events should have the day of the week field set to 1 - 7
+//	1=Sunday, 2=Monday, 3=Tuesday, 4=Wednesday, 5=Thurday, 6=Friday, 7=Saturday
+//
+// "F"loating events uses:
+//	the Month field for the Month.
+//	the Day field as the Cardinal Occurrence
+//		1=1st, 2=2nd, 3=3rd, 4=4th, 5=5th, 6=6th occurrence of the day listed next
+//	the Year field as the Day of the week the event/holiday falls on
+//		1=Sunday, 2=Monday, 3=Tuesday, 4=Wednesday, 5=Thurday, 6=Friday, 7=Saturday
+//	example: "F",	"1",	"3",	"2", = Floating holiday in January on the 3rd Monday of that month.
+//
+//	Note: Easter has it's own special formula so Please don't change anything related to Easter below
+//
+// "Y"early events are specific dates that never change - the Year field is ignored
+//	example - Christmas is: "12","25","",
+events_rider_after = new Array(
+	["",	"4",	"5",	"2016",	"1:00 AM",	"12:00 PM",	"Hospital",	"From : Sieble To : Carle"],
+	["",	"4",	"8",	"2016",	"1:00 AM",	"12:00 PM",	"Hospital",	"From : Sieble To : Carle"],
+	["",	"4",	"13",	"2016",	"1:00 AM",	"12:00 PM",	"Hospital",	"From : Sieble To : Carle"],
+	["",	"4",	"20",	"2016",	"1:00 AM",	"12:00 PM",	"Hospital",	"From : Sieble To : Carle"],
+	["",	"4",	"29",	"2016",	"1:00 AM",	"12:00 PM",	"Hospital",	"From : Sieble To : Carle"]
+// Please omit the final comma after the ] from the last line above unless you are going to add another event at this time.
+);
+
+
+
+
